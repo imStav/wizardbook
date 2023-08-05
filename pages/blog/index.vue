7dd1d604cf5742c1cc71a2983c617d7e11ec03ce
imStav <saprocontact@gmail.com>
2023-08-05 20:22:27 +0200

    refactor(pages): refactor blog page UI with unocss

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,10 +1,23 @@
 <template>
-  <div class="flex flex-col 2xl:w-1/2 w-11/12 md:w-2/3 mx-auto my-8">
-    <h1 class="text-5xl font-bold dark:text-slate-50">Le Grimoire</h1>
-    <p class="opacity-70 dark:text-slate-50">Un recueil de notes et de savoirs 🧪</p>
+  <div class="2xl:w-1/2 w-11/12 md:w-2/3 mx-auto my-8">
+    <h1
+      text="5xl"
+      font="display"
+    >
+      Le Grimoire
+    </h1>
+    <p 
+      class=""
+      text="blueGray"
+      font="sans"
+    >
+      Un recueil de notes et de savoirs 🧪
+    </p>
   </div>
 
-  <div class="fixed bottom-0 right-0 h-48 w-[3000px] bg-gradient-to-l from-amber-900/40 to-yellow-500/40 -z-10 blur-3xl"></div>
+  <!-- text-5xl font-display font-bold dark:text-slate-50 -mb-2 -->
+
+  <div class="fixed bottom-0 right-0 h-48 w-[3000px] bg-gradient-to-l from-amber-900/30 to-yellow-500/30 -z-10 blur-3xl"></div>
 
   <!-- <div class="fixed bottom-0 right-0 h-48 w-[3000px] bg-gradient-to-l from-indigo-900/50 to-fuchsia-500/50 -z-10 blur-3xl"></div> -->
 
@@ -14,12 +27,32 @@
       (./content/blog) and displays front-matter data (title, date...) 
     -->
     <ContentList path="/blog" v-slot="{ list }">
-      <div v-for="article in list" :key="article._path" class="p-4 border dark:border-slate-500/30 rounded-lg dark:bg-gradient-to-t dark:from-amber-800/20 dark:text-slate-200 dark:hover:bg-gradient-to-t dark:hover:from-amber-800/30 dark:hover:border-slate-500/50 duration-200 h-fit">
-          <div class="flex flex-col">
-            <h2 class="text-2xl font-semibold">{{ article.title }}</h2>
+      <div v-for="article in list" :key="article._path" class="">
+          <div 
+            class="p-4 rounded-lg backdrop-blur-2xl"
+            flex="~ col"
+            bg-gradient="to-t from-stone/10 to-blueGray/15"
+            font="sans"
+          >
+            <h2
+              text="2xl"
+              font="display semibold"
+            >
+              {{ article.title }}
+            </h2>
             
-            <div class="flex mt-1">
-              <p v-for="tag in article.tags" class="dark:bg-white/20 mr-2 px-2 rounded-full text-sm opacity-50">#{{ tag }}</p>
+            <div 
+              class="mt-1"
+              flex="~"
+            >
+              <p 
+                v-for="tag in article.tags" 
+                class="mr-2 px-2 rounded-full"
+                text="sm"
+                bg="blueGray/30 "
+              >
+                #{{ tag }}
+              </p>
             </div>
 
             <div class="my-4">
@@ -29,10 +62,29 @@
             
             <p class="">{{ article.description }}</p>
 
-            <img v-if="article.img" :src="article.img" alt="" class="rounded-lg my-4 w-full h-36 object-cover">
+            <img 
+              v-if="article.img" 
+              :src="article.img" 
+              alt="" 
+              class="rounded-lg my-4 w-full h-36 object-cover"
+            >
             
-            <NuxtLink :to="article._path" class="self-end w-fit border-b border-dotted duration-200 hover:text-white">Lire la suite</NuxtLink>
+            <div
+              class="hover:text-white mt-4 w-fit" 
+              flex="~ self-end items-center"
+            >
+              <NuxtLink 
+              :to="article._path"
+              text="blueGray hover:white"
+              decoration="none"
+              >
+                Lire la suite
+              </NuxtLink>
+              <div class="i-lucide-arrow-right ml-2"></div>
+            </div>
           </div>
+
+          <div class="my-6 sm:hidden"></div>
       </div>
     </ContentList>
   </main>
