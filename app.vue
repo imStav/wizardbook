c9decafbdc3a6126425651f2f89de7d3486feb68
imStav <saprocontact@gmail.com>
2023-08-05 19:44:54 +0200

    refactor(nav): refactor nav UI with unocss

--- app.vue
+++ app.vue
@@ -1,11 +1,70 @@
 <template>
   <nav class="sticky top-0 w-full py-4 backdrop-blur-2xl z-50">
-    <div class="w-fit mx-auto">
-      <NuxtLink to="/" class="hover:text-white duration-200 border-b border-dotted">Accueil</NuxtLink>
-      <NuxtLink to="/blog" class="mx-4 hover:text-white duration-200 border-b border-dotted">Grimoire</NuxtLink>
-      <NuxtLink to="/resources" class="hover:text-white duration-200 border-b border-dotted">Runes</NuxtLink>
+    <div 
+      class="w-fit mx-auto"
+      flex="~"
+    >
+      <div 
+        class="hover:text-white" 
+        flex="~ row items-center"
+      >
+        <NuxtLink 
+          to="/" 
+          class="hover:text-white"
+          text="xl blueGray"
+          font="sans"
+          decoration="none"
+        >
+          Accueil
+        </NuxtLink>
+        <div class="i-lucide-key ml-1"></div>
+      </div>
+      
+      <div 
+        class="hover:text-white mx-6" 
+        flex="~ row items-center"
+      >
+        <NuxtLink 
+          to="/blog" 
+          class="hover:text-white"
+          text="xl blueGray"
+          font="sans"
+          decoration="none"
+        >
+          Grimoire
+        </NuxtLink>
+        <div class="i-lucide-book-marked ml-1 rotate-10"></div>
+      </div>
+
+      <div 
+        class="hover:text-white" 
+        flex="~ row items-center"
+      >
+        <NuxtLink 
+          to="/resources" 
+          class="hover:text-white"
+          text="xl blueGray"
+          font="sans"
+          decoration="none"
+        >
+          Runes
+        </NuxtLink>
+        <div class="i-lucide-key ml-1"></div>
+      </div>
     </div>
   </nav>
 
   <NuxtPage />
 </template>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  background-color: black;
+  color: #bed5e9;
+}
+</style>
